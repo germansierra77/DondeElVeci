2ad503b670ff6b54582f29387398b01aa7dc0b08
germansierra77 <germansierra77@gmail.com>
2025-05-03 20:15:13 -0500

    Ajustes en  interfaces inicio de sesion y menu principal

--- js/main.js
+++ js/main.js
@@ -1,34 +1,23 @@
-/*FUNCION QUE SIRVE PARA DIRIGIRSE A MENU PRINCIPAL*/
+/*FUNCIONES DE BOTONES MENU PRINCIPAL*/
 document.addEventListener("click", (e)=> {
     if(e.target.matches("#btningreso"))
     location.href="seleccionusuario"
-})
-/*FUNCION QUE SIRVE PARA REGISTRARSE */
-document.addEventListener("click", (e)=> {
     if(e.target.matches("#btnregistrarse"))
     location.href="creacionusuario"
-})
-/*FUNCION QUE SIRVE PARA REESTABLECER CONTRASEÑA*/
-document.addEventListener("click", (e)=> {
     if(e.target.matches("#btnrecordar"))
     location.href="reestablecer"
 })
-/*FUNCION QUE SIRVE PARA GUARDAR DATOS*/
-document.addEventListener("click", (e)=> {
-    if(e.target.matches("#btnguardar"))
-    location.href="seleccionusuario"
-})
 /*FUNCION QUE SIRVE PARA VOLVER A INICIO DE SESION*/
 document.addEventListener("click", (e)=> {
-    if(e.target.matches("#btnvolver"))
+    if(e.target.matches("#btniniciosesion"))
     location.href="iniciosesion"
-})
-/*FUNCION QUE DIRIGE AL MENU CLIENTE*/
-document.addEventListener("click", (e)=> {
-    if(e.target.matches("#opcioncliente"))
+    if(e.target.matches("#btnmenutendero"))
+    location.href="menutendero"
+    if(e.target.matches("#btnmenuprincipal"))
     location.href="menuprincipal"
 })
-/*FUNCION QUE DIRIGE AL MENU TENDERO*/
+
+
 document.addEventListener("click", (e)=> {
     if(e.target.matches("#opciontendero"))
     location.href="menutendero"
