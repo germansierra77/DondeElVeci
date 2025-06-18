a81a0e24f4a55a198f16478b71c28db7ef7f8acf
germansierra77gmail.com <germansierra77@gmail.com>
2025-06-18 13:05:11 -0500

    Login validando credenciales por metodo POST

--- api/login/login.php
+++ api/login/login.php
@@ -1,22 +1,59 @@
 <?php
-    require_once "../configbd/db.php";
-    header("Access-control-Allow-Origin: *");
-    header("Content-Type: application/json");
+require_once "../configbd/db.php";
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
 
-    if($_SERVER["REQUEST_METHOD"] == "GET"){
-        $data= json_decode(file_get_contents("php://input"));
-    try {
-            $base = new Db();
-            $conn = $base->conectar();
-            header("HTTP/1.1 200 OK");
-            echo json_encode(array("code"=>200,"msg"=> "Conexion exitosa"));
-    } catch (Exception $e) {
-        header("HTTP/1.1 500 Internal Server Error");
-        echo json_encode(array("code"=>500,"msg" => "Error en el servidor /n".$e->getMessage()));
-    }
-    }else{
-        header("HTTP/1.1 400 Bad Request");
-        echo json_encode(array("code"=>400,"msg" => "Solicitud incorrecta por parte del cliente"));
-    }
+try {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $data = json_decode(file_get_contents("php://input"), true);
+        try {
+            // Validación de parámetros
+            if (isset($_GET['usuario']) && isset($_GET['clave'])) {
+                $base = new Db();
+                $conn = $base->conectar();
 
+                $user = $_POST['usuario'];
+                $pass = $_POST['clave'];
+
+                $sql = "SELECT `Id`, `Nombre`, `Apellido` FROM `tbusuarios` WHERE `Correo` = :user_name 
+                AND `Contrasena` =MD5(:password_user)";
+
+                $stmt = $conn->prepare($sql);
+                $stmt->bindValue(':user_name', trim($user), PDO::PARAM_STR);
+                $stmt->bindValue(':password_user', trim($pass), PDO::PARAM_STR);
+
+                if ($stmt->execute()) {
+                    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                    if (count($result) > 0) {
+                        $idUser = $result[0]["Id"];
+                        $userNombre = $result[0]["Nombre"] . " " . $result[0]["Apellido"];
+
+                        header("HTTP/1.1 200 OK");
+                        echo json_encode(["code" => 200,"idUser" => $idUser,"usuario" => $userNombre,
+                        "msg" => "Usuario validado OK"]);
+                    } else {
+                        header("HTTP/1.1 203 Non-Authoritative Information");
+                        echo json_encode(["code" => 203, "msg" => "Las credenciales no son válidas"]);
+                    }
+                } else {
+                    header("HTTP/1.1 500 Internal Server Error");
+                    echo json_encode(["code" => 500, "msg" => "Error al ejecutar la consulta"]);
+                }
+            } else {
+                header("HTTP/1.1 400 Bad Request");
+                echo json_encode(["code" => 400, "msg" => "Error, faltan parámetros requeridos"]);
+            }
+        } catch (Exception $e) {
+            header("HTTP/1.1 500 Internal Server Error");
+            echo json_encode(["code" => 500, "msg" => "Error en el servidor\n" . $e->getMessage()]);
+        }
+    } else {
+        header("HTTP/1.1 405 Method Not Allowed");
+        echo json_encode(["code" => 405, "msg" => "Método HTTP no permitido"]);
+    }
+} catch (Exception $e) {
+    header("HTTP/1.1 500 Internal Server Error");
+    echo json_encode(["code" => 500, "msg" => "Error en el servidor\n" . $e->getMessage()]);
+}
 ?>
